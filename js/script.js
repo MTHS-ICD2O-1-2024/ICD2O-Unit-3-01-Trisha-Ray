0485411ada5a5d135ec7ccb96506e7a349b8c0b0
default <trisha.ray@stu.ocsb.ca>
2025-03-27 15:50:16 +0000

    template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Trisha Ray
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculateAreaOfTriangle () {
+  // input
+  const baselength = parseInt(document.getElementById('base-length').value)
+  const heightlength = parseInt(document.getElementById('height-length').value)
+
+  // process
+  const areaOfTriangle = (baselength * heightlength) / 2
+
+  // output
+  document.getElementById('area').innerHTML = 'Area of triangle is: ' + areaOfTriangle + ' cm²'
+}
